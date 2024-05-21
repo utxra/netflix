35e2bd4323bff801e97e176664f825b336b37ef5
Francisco Javier Ruiz Avilés <ruizaviles.franciscojavier@loscerros.org>
2024-05-21 17:30:31 +0200

    EmailTest con Resend -> Working

--- src/Controller/emailtest.php
+++ src/Controller/emailtest.php
@@ -1,7 +1,7 @@
 <?php
 
 // Include Composer autoload file to load Resend SDK classes...
-require_once 'C:\Users\alumno\Documents\proyecto\netflix\vendor\autoload.php';
+include_once './vendor/autoload.php';
 
 // Assign a new Resend Client instance to $resend variable, which is automatically autoloaded...
 $resend = Resend::client('re_TPPKWAXB_54DKvHzkZi2PArJyAabbQF4C');
@@ -9,7 +9,7 @@ $resend = Resend::client('re_TPPKWAXB_54DKvHzkZi2PArJyAabbQF4C');
 try {
     $result = $resend->emails->send([
         'from' => 'Acme <onboarding@resend.dev>',
-        'to' => ['ruizaviles.franciscojavier@loscerros.org'],
+        'to' => ['trxpute@gmail.com'],
         'subject' => 'Hello world',
         'html' => '<strong>It works!</strong>',
     ]);
