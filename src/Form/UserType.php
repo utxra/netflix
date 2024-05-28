02d69ba5a6acee3b759956bf76ef113917a7d8ad
utxra <trxpute@gmail.com>
2024-05-28 13:21:49 +0200

    CRUD user ✅ NavBar ✅ base.html.twig

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
@@ -13,7 +14,17 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', 
+                ChoiceType::class, 
+                [
+                    'choices' => [
+                        'User' => 'ROLE_USER',
+                        'Admin' => 'ROLE_ADMIN',
+                    ],
+                    'multiple' => true,
+                    'expanded' => true,
+                    'disabled' => true,
+                ])
             ->add('password')
             ->add('isVerified')
             ->add('nombre')
