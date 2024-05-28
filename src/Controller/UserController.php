20de4f614586624d371e9e1eba77cb05474b5ea0
utxra <trxpute@gmail.com>
2024-05-28 11:36:57 +0200

    Navbar ❕ - Login ✅

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+
 #[IsGranted('ROLE_ADMIN')]
 #[Route('/admin/user')]
 class UserController extends AbstractController
