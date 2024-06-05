bd23b8cb4744bc0954a521e81244e57af059bff4
Francisco Javier Ruiz Avilés <ruizaviles.franciscojavier@loscerros.org>
2024-06-06 01:39:02 +0200

    EMAIL DE CONFIRMACION ✅ / BLOQUEAR NO VERIFICADOS⚠⚠/RESET PASSWORD ✅

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,26 +5,36 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\EmailVerifier;
+use Doctrine\Common\Lexer\Token;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
+use Resend;
 
 class RegistrationController extends AbstractController
 {
-    public function __construct(private EmailVerifier $emailVerifier)
+    private $entityManager;
+    private $tokenGenerator;
+
+    public function __construct(EntityManagerInterface $entityManager, TokenGeneratorInterface $tokenGenerator)
     {
+            $this->entityManager = $entityManager;
+            $this->tokenGenerator = $tokenGenerator;
     }
 
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager, MailerInterface $mailer): Response    
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_home');
@@ -49,66 +59,42 @@ class RegistrationController extends AbstractController
 
             $user->setAvatar($form->get('avatar')->getData());
 
+            // generate a verification token
+            $user->setVerificationToken($this->tokenGenerator->generateToken());
+
             // Add some logging
             error_log(print_r($user, true));
+            error_log(print_r($user->getVerificationToken(), true));
+
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation(
-                'app_verify_email',
-                $user,
-                (new TemplatedEmail())
-                    ->from(new Address('mailer@franruiz.dev', 'Netflix Mail Bot'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-/*
-            // do anything else you need here, like send an email
-            // Include Composer autoload file to load Resend SDK classes...
-            include_once '../../vendor/autoload.php';
+            // generate the template email with Twig
 
-            // Assign a new Resend Client instance to $resend variable, which is automatically autoloaded...
+            $emailHtml = $this->renderView('registration/confirmation_email.html.twig', [
+                'user' => $user->getNombre(),
+                'token' => $user->getVerificationToken()
+            ]);
+            
+
             $resend = Resend::client('re_TPPKWAXB_54DKvHzkZi2PArJyAabbQF4C');
+            
+            
+            $result = $resend->emails->send([
+                'from' => 'Netflix Mail Bot <noreply@franruiz.dev>',
+                'to' => [$user->getEmail()],
+                'subject' => 'Confirma tu correo electrónico',
+                'html' => $emailHtml,
+            ]);
 
-            try {
-                $result = $resend->emails->send([
-                    'from' => 'Administracion <noreply@franruiz.dev>',
-                    'to' => ['trxpute@gmail.com'],
-                    'subject' => 'Hello world',
-                    'html' => '<strong>It works!</strong>',
-                ]);
-            } catch (\Exception $e) {
-                exit('Error: ' . $e->getMessage());
-            }
-*/
+            // do anything else you need here, like send an email
+
             return $security->login($user, 'form_login', 'main');
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
         ]);
-    }
-
-    #[Route('/verify/email', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
-    {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-        // validate email confirmation link, sets User::isVerified=true and persists
-        try {
-            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
-        } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
-
-            return $this->redirectToRoute('app_register');
-        }
-
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
-
-        return $this->redirectToRoute('app_home');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -186,14 +186,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // $this->plainPassword = null;
     }
 
-    public function isVerified(): bool
+    public function getIsVerified(): bool
     {
         return $this->isVerified;
     }
 
-    public function setVerified(bool $isVerified): static
+    public function setIsVerified(bool $verification): static
     {
-        $this->isVerified = $isVerified;
+        $this->isVerified = $verification;
 
         return $this;
     }
